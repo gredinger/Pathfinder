cccbe0acf4ca4e3a15439e01202e04d3ab2c20fc
Gredinger <gene.redinger@gmail.com>
2016-03-09 21:40:06 -0500

    Added namespace with Pathfinder.php

---
+++ Framework/Pathfinder.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Gene
+ * Date: 3/9/2016
+ * Time: 9:38 PM
+ */
+namespace Pathfinder {
+    include '../Class/Town.php';
+    include "../Class/World.php";
+    include "../generate.php"
+}
